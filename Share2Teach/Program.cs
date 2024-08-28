766fd7998260b423bb55796f80f543c8949c2bca
Cajee7 <muhammedcajee29@gmail.com>
2024-08-29 01:46:38 +0200

    Added the mongoDB dependancy

--- Share2Teach/Program.cs
+++ Share2Teach/Program.cs
@@ -1,44 +1,58 @@
-var builder = WebApplication.CreateBuilder(args);
+using System;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace MongoDBExample
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    // Define the data model
+    public class User
+    {
+        public ObjectId Id { get; set; } // MongoDB uses ObjectId by default for _id
+        public string FName { get; set; }
+        public string LName { get; set; }
+    }
 
-app.UseHttpsRedirection();
+    // MongoDB service class
+    public class MongoDBService
+    {
+        private readonly IMongoDatabase _database;
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Super Hot",
-};
+        public MongoDBService(string connectionString, string databaseName)
+        {
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+        public IMongoCollection<User> GetUsersCollection()
+        {
+            return _database.GetCollection<User>("Users");
+        }
+    }
 
-app.Run();
+    // Main program class
+    public class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            // Connection string and database name
+            string connectionString = "mongodb+srv://muhammedcajee29:RU2AtjQc0d8ozPdD@share2teach.vtehmr8.mongodb.net/";
+            string databaseName = "share2teach"; // Specify the database name here
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+            // Initialize MongoDB service
+            var mongoDbService = new MongoDBService(connectionString, databaseName);
+
+            // Get the collection
+            var usersCollection = mongoDbService.GetUsersCollection();
+
+            // Query to find all documents
+            var users = await usersCollection.Find(_ => true).ToListAsync();
+
+            // Display FName and LName
+            foreach (var user in users)
+            {
+                Console.WriteLine($"First Name: {user.FName}, Last Name: {user.LName}");
+            }
+        }
+    }
 }
