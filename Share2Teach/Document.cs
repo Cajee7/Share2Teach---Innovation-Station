1e40a69cecf39fd7e01bf4c8601c1ce33abaf454
Cajee7 <muhammedcajee29@gmail.com>
2024-08-29 23:45:17 +0200

    Inital work with documents

---
+++ Share2Teach/Document.cs
@@ -0,0 +1,61 @@
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System;
+using DatabaseConnection;
+
+namespace Documents
+{
+    public class Documents
+    {
+        //setting max file size to 25mbs
+        private const long MaxFileSize = 25 * 1024 * 1024;
+
+        //method to upload document and check file size
+        public static void UploadDocument(string filePath)
+        {
+            if(File.Exists(filePath))
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
+
+                //checking file size
+                if(fileInfo.Length <= MaxFileSize)
+                {
+                    //Remove
+                    //Giving file size
+                    Console.WriteLine("File within size limit.");
+                }
+                else
+                {
+                    Console.WriteLine("Error: File size exceeds 25 MB!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: File not found!");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            //connecting to database
+            var database = Program.ConnectToDatabase();
+
+            if(database != null)
+            {
+                //lines for testing purposes
+                //To be removed
+                Console.WriteLine("Success! Connected to database");
+
+                //Api needed but for time being using file path
+                //Remove
+                string filePath = @"";
+
+                UploadDocument(filePath);
+            }
+            else
+            {
+                Console.WriteLine("Document page failed to connect to database!");
+                Console.WriteLine("Please refresh page and if the problem persists, try again later!");
+            }
+        }
+    }
+}
