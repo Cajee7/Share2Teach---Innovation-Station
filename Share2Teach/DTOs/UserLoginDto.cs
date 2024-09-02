aed10d6100e4d2c56404f1666138b2a82fac50fb
zahrakara03 <143691458+zahrakara03@users.noreply.github.com>
2024-09-02 13:57:42 +0200

    Created models and DTOs

---
+++ Share2Teach/DTOs/UserLoginDto.cs
@@ -0,0 +1,5 @@
+public class UserLoginDto
+{
+    public required string Email { get; set; }    // Required property for the user's email
+    public required string Password { get; set; } // Required property for the user's password
+}
---
+++ Share2Teach/DTOs/UserRegistrationDto.cs
@@ -0,0 +1,9 @@
+public class UserRegistrationDto
+{
+    public required string FName { get; set; }          // Required property for the user's first name
+    public required string LName { get; set; }          // Required property for the user's last name
+    public required string Email { get; set; }          // Required property for the user's email
+    public required string Password { get; set; }       // Required property for the user's password
+    public required string ConfirmPassword { get; set; } // Required property to confirm the password
+    public required string Subjects { get; set; }   // Required property for the subjects the educator teaches 
+}
