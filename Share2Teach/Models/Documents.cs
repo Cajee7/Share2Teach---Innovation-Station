19d8c258dd584672af0d0206408d396c303d52b2
Cajee7 <muhammedcajee29@gmail.com>
2024-09-21 17:58:25 +0200

    DocUpload and model draft 1

--- Share2Teach/Models/Documents.cs
+++ Share2Teach/Models/Documents.cs
@@ -1,18 +1,19 @@
-using System;
+using MongoDB.Bson;
 
 namespace Document_Model.Models
 {
     public class Documents
     {
-        //Database info required
-        public string Title { get; set; }
-        public string Subject { get; set; }
+        public ObjectId Id {get; set;} // MongoDB ID
+        public required string Title { get; set; }
+        public required string Subject { get; set; }
         public int Grade { get; set; }
-        public string Description { get; set; }
-        public string ModerationStatus { get; set; } 
-        public DateTime DateUploaded { get; set; } 
-        public int Ratings { get; set; }
-        public List<string> Tags { get; set; } 
-        public double FileSize { get; set; } 
+        public required string Description { get; set; }
+        public double FileSize { get; set; }
+        public required string FileUrl {get; set;}
+        public string ModerationStatus {get; set;} = "Unmoderated";
+        public int Ratings {get; set;}
+        public List<string> Tags {get; set;} = new(); // Default to an empty list
+        public DateTime DateUploaded { get; set; }
     }
 }
