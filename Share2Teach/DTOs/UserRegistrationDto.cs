cfafd5393764f310fb60cb393771ac9439c81efe
zahrakara03 <143691458+zahrakara03@users.noreply.github.com>
2024-09-22 23:23:53 +0200

    Added validation for teacher signup

--- Share2Teach/DTOs/UserRegistrationDto.cs
+++ Share2Teach/DTOs/UserRegistrationDto.cs
@@ -1,30 +1,33 @@
-using System.ComponentModel.DataAnnotations; // Include the namespace for validation attributes
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 public class UserRegistrationDto
 {
-    [Required(ErrorMessage = "First name is required.")] // Ensures the first name is provided
-    [StringLength(15, MinimumLength = 2, ErrorMessage = "First name must be between 2 and 15 characters.")] // Sets min and max length of the first name
-    public string FName { get; set; }         
+    [Required(ErrorMessage = "First name is required.")]
+    [StringLength(15, MinimumLength = 2, ErrorMessage = "First name must be between 2 and 15 characters.")]
+    public string FName { get; set; }
 
-    [Required(ErrorMessage = "Last name is required.")] // Ensures the last name is provided
-    [StringLength(20, MinimumLength = 2, ErrorMessage = "Last name must be between 2 and 20 characters.")] // Sets min and max length of the last name
-    public string LName { get; set; }          
+    [Required(ErrorMessage = "Last name is required.")]
+    [StringLength(20, MinimumLength = 2, ErrorMessage = "Last name must be between 2 and 20 characters.")]
+    public string LName { get; set; }
 
-    [Required(ErrorMessage = "Email is required.")] // Ensures the email is provided
-    [EmailAddress(ErrorMessage = "Invalid email format.")] // Validates the email format
-    public string Email { get; set; }          
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
+    public string Email { get; set; }
 
-    [Required(ErrorMessage = "Password is required.")] // Ensures the password is provided
-    [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")] // Sets a minimum length for the password
-    [DataType(DataType.Password)] // Specifies that this field is a password
-    public string Password { get; set; }       
+    [Required(ErrorMessage = "Password is required.")]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
 
-    [Required(ErrorMessage = "Confirmation password is required.")] // Ensures the confirmation password is provided
-    [Compare("Password", ErrorMessage = "Passwords do not match.")] // Validates that the confirmation password matches the password
-    [DataType(DataType.Password)] // Specifies that this field is a password
-    public string ConfirmPassword { get; set; } 
+    [Required(ErrorMessage = "Confirmation password is required.")]
+    [Compare("Password", ErrorMessage = "Passwords do not match.")]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; }
 
-   [Required(ErrorMessage = "At least one subject is required.")]
-   [MinLength(1, ErrorMessage = "You must provide at least one subject.")]
-    public List<string> Subjects { get; set; } = new List<string>(); 
+    // Default role is "User"
+    public string Role { get; set; } = "User";
+
+    // Only required for teachers
+    public List<string> Subjects { get; set; } = new List<string>();
 }
