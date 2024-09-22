12cb4a0a473b0fa1e7f9c6e5e7603e5fb472d127
Angelina Jaedene Ramsunar <91144409+aramsunar@users.noreply.github.com>
2024-09-22 19:37:14 +0200

    Changes to fix error in ReportDto

--- Share2Teach/DTOs/ReportDto.cs
+++ Share2Teach/DTOs/ReportDto.cs
@@ -1,9 +1,9 @@
 public class ReportDto
     {
         public int Report_id { get; set; }
-        public string DocumentId { get; set; }
-        public string UserId { get; set; }
-        public string Reason { get; set; }
-        public string Comment { get; set; }
-        public string Subject { get; set; }
+        public required string DocumentId { get; set; }
+        public required string UserId { get; set; }
+        public  required string Reason { get; set; }
+        public required string Comment { get; set; }
+        public required string Subject { get; set; }
     }
