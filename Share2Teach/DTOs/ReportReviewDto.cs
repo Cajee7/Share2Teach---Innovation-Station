7876399a1fcc885721f46e1d616b08ffff1b2b9a
Angelina Jaedene Ramsunar <91144409+aramsunar@users.noreply.github.com>
2024-09-22 19:23:16 +0200

    Added ReportReviewDTO file and code

---
+++ Share2Teach/DTOs/ReportReviewDto.cs
@@ -0,0 +1,5 @@
+public class ReportReviewDto
+    {
+        public bool Approve { get; set; } // Indicates whether the report is approved or denied
+        public string ModeratorComments { get; set; } // Comments from the moderator
+    }
