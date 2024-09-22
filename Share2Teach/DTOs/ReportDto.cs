3e4005e9df62ac8462030899f6eafc9f17e32cd7
Angelina Jaedene Ramsunar <91144409+aramsunar@users.noreply.github.com>
2024-09-22 19:18:03 +0200

    Added ReportDto file and code

---
+++ Share2Teach/DTOs/ReportDto.cs
@@ -0,0 +1,9 @@
+public class ReportDto
+    {
+        public int Report_id { get; set; }
+        public string DocumentId { get; set; }
+        public string UserId { get; set; }
+        public string Reason { get; set; }
+        public string Comment { get; set; }
+        public string Subject { get; set; }
+    }
