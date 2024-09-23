6abbfcb8ede4841021c05410f7c06e59c7dfea01
Angelina Jaedene Ramsunar <91144409+aramsunar@users.noreply.github.com>
2024-09-23 21:52:46 +0200

    Changes

--- Share2Teach/DTOs/ReportDto.cs
+++ Share2Teach/DTOs/ReportDto.cs
@@ -1,9 +1,11 @@
+using MongoDB.Bson;
+
 public class ReportDto
     {
         public int Report_id { get; set; }
-        public required string DocumentId { get; set; }
-        public required string UserId { get; set; }
+        public int DocumentId { get; set; }
+        public int UserId { get; set; }
         public  required string Reason { get; set; }
-        public required string Comment { get; set; }
+        //public required string Comment { get; set; }
         public required string Subject { get; set; }
     }
