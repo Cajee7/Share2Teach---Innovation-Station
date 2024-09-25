e7e4fbe4445b6e1d361d45e672c6e66b795803d9
Angelina Jaedene Ramsunar <91144409+aramsunar@users.noreply.github.com>
2024-09-25 14:12:48 +0200

    Added a saveReportDto.cs

---
+++ Share2Teach/DTOs/SaveReportDto.cs
@@ -0,0 +1,10 @@
+// DTO for saving report
+using MongoDB.Bson;
+
+public class SaveReportDto
+    {
+        public required string  DocumentId { get; set; }
+        public required string Reason { get; set; }
+
+        public required string Report_status{get;set;}
+    }
