1f50b29e04992e1376695b9d90b54c8de2f6cf94
ripatel04 <rp009108@gmail.com>
2024-09-27 03:40:45 +0200

    moderation testing

--- Share2Teach/Controllers/FileModeration.cs
+++ Share2Teach/Controllers/FileModeration.cs
@@ -14,7 +14,7 @@ namespace FileModeration.Controllers
     [Route("api/[controller]")] // Defines the base route for this controller as 'api/moderation'
     public class ModerationController : ControllerBase
     {
-        // MongoDB collections for documents and moderation entries
+        // MongoDB collections for documents and moderation entry's
         private readonly IMongoCollection<Documents> _documentsCollection;
         private readonly IMongoCollection<ModerationEntry> _moderationCollection;
 
