b0c43e3287467af55e04a7061abeb249fdf11a33
Angelina Jaedene Ramsunar <91144409+aramsunar@users.noreply.github.com>
2024-10-02 14:31:43 +0200

    Fixed the Yellow lines in the FileDownloadDto.cs

--- Share2Teach/DTOs/FileDowloadDto.cs
+++ Share2Teach/DTOs/FileDowloadDto.cs
@@ -1,5 +1,13 @@
+using Microsoft.AspNetCore.Http;
 
-    public class FileUploadDto
+/// <summary>
+/// Data transfer object for uploading a file.
+/// </summary>
+public class FileUploadDto
 {
+    /// <summary>
+    /// Gets or sets the file to be uploaded.
+    /// This field is required.
+    /// </summary>
     public required IFormFile File { get; set; }
 }
